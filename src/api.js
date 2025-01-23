37d73787dee342ce1c2fa36493fd394a3306fbcb
jansforte <johan.fuentes01@uceva.edu.co>
2025-01-23 08:25:21 -0500

    con dirección de renedr

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000'; // Cambia a la URL de tu backend
+const API_URL = 'https://server-influencers.onrender.com/'; // Cambia a la URL de tu backend
 
 export const fetchClaims = async (username, start_time, end_time) => {
     const filter = {
