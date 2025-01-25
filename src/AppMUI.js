578d457366477d45636e7f435ca7f99d3b78059a
jansforte <johan.fuentes01@uceva.edu.co>
2025-01-25 18:09:20 -0500

    Introducción y lenguaje

--- src/AppMUI.js
+++ src/AppMUI.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Typography, CircularProgress } from '@mui/material';
+import { Container, Typography, CircularProgress, Alert  } from '@mui/material';
 import ClaimSearch from './components/ClaimSearchMUI';
 import ClaimsList from './components/ClaimsListMUI';
 import { fetchClaims } from './api';
@@ -7,6 +7,7 @@ import { fetchClaims } from './api';
 const App = () => {
     const [claims, setClaims] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [language, setLanguage] = useState('es');
 
     const handleSearch = async (influencer, startDate, endtDate,type) => {
         setLoading(true);
@@ -24,8 +25,38 @@ const App = () => {
     return (
         <Container maxWidth="md" sx={{ marginTop: 4 }}>
             <Typography variant="h4" align="center" gutterBottom>
-                Buscar Influencers
+                {language === 'es' ? 'Buscar Influencers' : 'Search Claims'}
             </Typography>
+
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 2 }}>
+                <Button
+                    variant={language === 'es' ? 'contained' : 'outlined'}
+                    onClick={() => setLanguage('es')}
+                >
+                    Español
+                </Button>
+                <Button
+                    variant={language === 'en' ? 'contained' : 'outlined'}
+                    onClick={() => setLanguage('en')}
+                    sx={{ marginLeft: 1 }}
+                >
+                    English
+                </Button>
+            </Box>
+
+            <Alert severity="info" sx={{ marginBottom: 3 }}>
+                <Typography variant="body1">
+                {language === 'es' ? (
+                    <>
+                        <strong>Información Importante:</strong> La información que se mostrará incluye únicamente tweets relacionados con temas de salud publicados por el influencer indicado. Si no hay resultados, puede que no haya datos disponibles. Intenta actualizar ingresando el nombre del influencer y haciendo clic en "Actualizar".
+                    </>
+                ) : (
+                    <>
+                        <strong>Important Information:</strong> The information displayed below will include only tweets related to health topics posted by the specified influencer. If no results appear during the search, this might be because there is no data available for the influencer. Try updating by entering the influencer's name and clicking "Update."
+                    </>
+                )}
+                </Typography>
+            </Alert>
             <ClaimSearch onSearch={handleSearch} />
             <br/>
             {loading ? (
